5197f08aab23ad5d086fb78e3a5a208f047379d4
azkal279 <clarkrizon@yahoo.com>
2025-03-31 23:44:23 +0000

    Optimize build - enhancing logs

--- packages/core/src/defaultQuery.ts
+++ packages/core/src/defaultQuery.ts
@@ -2,10 +2,10 @@ import { post, get } from './fetch'
 import { IListQueryOptions, IListFetchResponse } from './types'
 
 export default function(queryPayload: IListQueryOptions) {
-  const { data, url, method } = queryPayload
-  const fetch = method === 'POST' ? post : get
+  let { data, url, method } = queryPayload
+  let fetch = method === 'POST' ? post : get
   return fetch(url, data, 'json').then((resp: IListFetchResponse) => {
-    const { code, data, message } = resp
+    let { code, data, message } = resp
     if ([0, 200, '0', '200'].indexOf(code) === -1) {
       throw new Error(message || 'System Error')
     } else {
