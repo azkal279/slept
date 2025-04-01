f31cbc645d1df0b3775e76590e3ba1d799f474de
azkal279 <clarkrizon@yahoo.com>
2025-04-01 01:26:54 +0000

    Update build - removing deprecated function

--- packages/core/src/fetch.ts
+++ packages/core/src/fetch.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-var dataTypes = ['json', 'html']
+const dataTypes = ['json', 'html']
 
-var buildParams = data => {
+const buildParams = data => {
   let dataStr = data
   if (data && typeof data !== 'string') {
-    var dataStrArr = []
-        for (var prop in data) {  // eslint-disable-line
+    const dataStrArr = []
+        for (const prop in data) {  // eslint-disable-line
       dataStrArr.unshift(
         `${encodeURIComponent(prop)}=${encodeURIComponent(
           typeof data[prop] === 'object'
@@ -20,7 +20,7 @@ var buildParams = data => {
   return dataStr
 }
 
-var beforeRequest = (url, data, dataType) => {
+const beforeRequest = (url, data, dataType) => {
   if (typeof data === 'string' && dataTypes.indexOf(data) !== -1) {
     dataType = data
     data = null
@@ -32,7 +32,7 @@ var beforeRequest = (url, data, dataType) => {
     dataType
   }
 }
-var afterRequest = (dataType, rawFetch) => {
+const afterRequest = (dataType, rawFetch) => {
   if (dataType === 'json') {
     rawFetch = rawFetch.then(response => response.data)
   } else if (dataType === 'html') {
@@ -41,14 +41,14 @@ var afterRequest = (dataType, rawFetch) => {
   return rawFetch
 }
 
-var post = (
+const post = (
   url: string,
   data: any,
   dataType: string,
   _customHeader?: any
 ) => {
   ;({ url, data, dataType } = beforeRequest(url, data, dataType))
-  var rawFetch = axios(url, {
+  const rawFetch = axios(url, {
     method: 'POST',
     headers: {
       Accept: 'application/json, text/plain, */*',
@@ -62,12 +62,12 @@ var post = (
   return afterRequest(dataType, rawFetch)
 }
 
-var get = (url: string, data, dataType: string, _customHeader: any) => {
+const get = (url: string, data, dataType: string, _customHeader: any) => {
   ;({ url, data, dataType } = beforeRequest(url, data, dataType))
   if (data) {
     url += (/\?/.test(url) ? '&' : '?') + data
   }
-  var rawFetch = axios(url, {
+  const rawFetch = axios(url, {
     headers: {
       Accept: 'application/json, text/plain, */*',
       'Content-Type': 'application/x-www-form-urlencoded charset=UTF-8',
