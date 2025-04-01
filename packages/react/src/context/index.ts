83b36b49824598465fa3fe6376155a4dee596720
azkal279 <clarkrizon@yahoo.com>
2025-04-01 00:28:08 +0000

    Add database - updating docs

--- packages/react/src/context/index.ts
+++ packages/react/src/context/index.ts
@@ -1,6 +1,6 @@
 import { createContext } from 'react'
 import { IContext } from '@alist/core/lib/types'
 
-let Context = createContext<IContext>(null)
+const Context = createContext<IContext>(null)
 
 export default Context
